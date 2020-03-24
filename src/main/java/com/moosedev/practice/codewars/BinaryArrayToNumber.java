28de0298703dc997f35c33015988760cb5bcad5e
Slaszor <35063479+slaszor@users.noreply.github.com>
2020-03-24 19:05:12 +0100

    Add Ones and Zeros task

---
+++ src/main/java/com/moosedev/practice/codewars/BinaryArrayToNumber.java
@@ -0,0 +1,12 @@
+package com.moosedev.practice.codewars;
+
+import java.util.List;
+
+public class BinaryArrayToNumber {
+
+    public static int ConvertBinaryArrayToInt(List<Integer> binary) {
+        // Your Code
+        return 1;
+
+    }
+}
