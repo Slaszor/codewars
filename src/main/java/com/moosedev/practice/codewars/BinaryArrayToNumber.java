5be6da5eeb0a2207a748d64ef888703f8e4134db
Slaszor <35063479+slaszor@users.noreply.github.com>
2020-03-24 19:29:25 +0100

    Finished working answer

--- src/main/java/com/moosedev/practice/codewars/BinaryArrayToNumber.java
+++ src/main/java/com/moosedev/practice/codewars/BinaryArrayToNumber.java
@@ -1,12 +1,19 @@
 package com.moosedev.practice.codewars;
 
+import java.util.Collections;
 import java.util.List;
 
 public class BinaryArrayToNumber {
 
     public static int ConvertBinaryArrayToInt(List<Integer> binary) {
-        // Your Code
-        return 1;
+        Collections.reverse(binary);
+        Integer power = 0;
+        Integer output = 0;
 
+        for ( Integer i : binary){
+            if (i==1) output += (int) Math.pow(2, power);
+            power++;
+        }
+        return output;
     }
 }
